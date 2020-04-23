5e1b3b7c5eee0fc705afe91f266cbb400bf3e35c
Jai Parmar <jaiparmar207@gmail.com>
2020-04-23 15:26:42 +0530

    updated the index route module

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+
 //Set up mongoose connection
 var mongoose = require('mongoose');
 
@@ -24,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
